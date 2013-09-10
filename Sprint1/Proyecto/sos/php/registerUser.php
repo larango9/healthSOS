c160cef87f60f90ee29cadca7ab95ca7ffd336c3
Leydi Arango <larango9@eafit.edu.co>
2013-09-09 20:04:30 -0500

    Nuevas modificaciones del códiigo
    se modifico el codigo para facilitar las conexiones.

---
+++ Sprint1/Proyecto/sos/php/registerUser.php
@@ -0,0 +1,24 @@
+<?php
+    
+     include_once 'Session.php';
+     include_once 'MysqlDBC.php';
+
+        $numDocument = $_REQUEST['numDocument'];
+        $pass = $_REQUEST['pass'];
+        $userName = $_REQUEST['userName'];
+        $userSurname = $_REQUEST['userSurname'];
+        $birthday = $_REQUEST['birthday'];
+        $email = $_REQUEST['email'];
+        $cellNum = $_REQUEST['cellNum'];
+     
+        $sql = "INSERT INTO `user` (`id_user`, `numDocument`, `password`, `userName`, `userSurname`, `birthday`, `email`, `cellNum`) VALUES (NULL, '" . $numDocument . "', '" . $pass . "', '" . $userName . "', '" . $userSurname . "', '" . $birthday . "', '" . $email . "', '" . $cellNum . "') ";
+
+        $mysqlDBC = new MysqlDBC();
+        $result = $mysqlDBC->insert($sql);
+        if ($result != null) {
+            createSession($numDocument);
+            echo json_encode(array('result' => true));
+        } else {
+            echo json_encode(array('result' => false));
+        }
+?>
