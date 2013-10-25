e7281b594bbb06f5442d5503e0888654a703e058
Juan Pablo Urango Vitola <jupaurvi@gmail.com>
2013-10-24 21:03:15 -0500

    Se agrega proyecto en ruby on rails

---
+++ Ruby/HealthSOS/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthSOS::Application.config.secret_token = '2a3ccc6a6c514fb946c1bc80d8852900109c6fc34dd356d2f2b8716f750356b7fa3ac85a56a401144b2c83a4c64e322169f283bdfc1ba2c38927b866da666ef1'
